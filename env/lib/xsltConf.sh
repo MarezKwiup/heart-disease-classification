1f74ba03575b8f0f631b8237d24ed2aa5289a526
MarezKwuip <marezuchiha@gmail.com>
2024-05-24 10:40:40 +0530

    First commit

---
+++ env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/vardansharma/Desktop/heart-disease-project/env/lib"
+XSLT_LIBS="-lxslt -L/Users/vardansharma/Desktop/heart-disease-project/env/lib -lxml2 -L/Users/vardansharma/Desktop/heart-disease-project/env/lib -L/Users/vardansharma/Desktop/heart-disease-project/env/lib -lpthread -L/Users/vardansharma/Desktop/heart-disease-project/env/lib -liconv -L/Users/vardansharma/Desktop/heart-disease-project/env/lib -licui18n -licuuc -licudata -lm "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/vardansharma/Desktop/heart-disease-project/env/include"
+MODULE_VERSION="xslt-1.1.35"
